09eec70f07d6e872caf2106b5d905692e67b95b5
MarlonStingelin <151676126+MarlonStingelin@users.noreply.github.com>
2026-06-29 12:46:07 -0300

    etapa 3 Update 4/5

--- main.hs
+++ main.hs
@@ -3,15 +3,33 @@ module Main where
 import Lexer
 import Parser
 import Semantico
+import Gerador
+import AST
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
     src <- getContents
+    let nomeClasse = "Programa"
+
     let ast = parser (alexScanTokens src)
+
     let (temErro, msgs, astAnotada) = analisaSemântica ast
+
     if null msgs
-        then putStrLn "Analise semantica concluida sem erros ou advertencias."
-        else putStr msgs
+        then return ()
+        else hPutStrLn stderr msgs
+
     if temErro
-        then putStrLn "Analise semantica concluida sem erros ou advertencias."
-        else print astAnotada
+        then hPutStrLn stderr "Compilacao abortada: corrija os erros semanticos."
+        else do
+            let tblGlobal = buildTblGlobal astAnotada
+            let jasmin = gerar nomeClasse astAnotada tblGlobal
+            putStr jasmin
+
+buildTblGlobal :: Programa -> [(Id, ([Tipo], Tipo))]
+buildTblGlobal (Prog funcoes _ _ _) =
+    map entry funcoes
+  where
+    entry (nome :->: (params, ret)) = (nome, (map varTipo params, ret))
+    varTipo (_ :#: (t, _)) = t
